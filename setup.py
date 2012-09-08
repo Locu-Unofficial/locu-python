e6897892644e64ea5b1d86be496047f0be34b4dc
Kainar Kamalov <kainar.kamalov@gmail.com>
2012-09-08 10:49:27 +0000

    adding pypi support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'locu-python',
+    packages = ['locu-python'], 
+    version = '0.1',
+    description = 'Unofficial Locu Python API Wrapper',
+    author = 'Kainar Kamalov',
+    author_email = 'kainar.kamalov@gmail.com',
+    url = 'https://github.com/Locu-Unofficial/locu-python',
+    download_url = 'https://github.com/Locu-Unofficial/locu-python/tarball/0.1',
+    keywords = ['locu-api', 'api', 'locu'], 
+    classifiers = [],  
+)
