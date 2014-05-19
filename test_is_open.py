7854baa223bb65ba6904fba33d55879b565bfb6d
Tanner Baldus <t.baldus285@gmail.com>
2014-05-19 14:21:13 -0700

    made changes in Jason's comments on pull request

--- test_is_open.py
+++ test_is_open.py
@@ -5,18 +5,18 @@ import unittest
 global KEY
 
 
-class IsOpenTest(object):
+class IsOpenTest(unittest.TestCase):
 
 	def test_no_data(self):
 		venue_client = VenueApiClient(KEY)
 		venue_id = "50d70de7fbafb112c2de" ##no data
-		self.assertEqual(venue_client.is_open(venue_id,"12:00:00","Monday"))
+		self.assertIsNone(venue_client.is_open(venue_id,"12:00:00","Monday"))
 
 
 	def test_open(self):
 		venue_client = VenueApiClient(KEY)
 		venue_id='b7b1644a6bb10dff58bd' ## open Monday 09:00:00 - 15:00:00
-		self,assertTrue(venue_client.is_open(venue_id,"12:00:00","Monday"))
+		self.assertTrue(venue_client.is_open(venue_id,"12:00:00","Monday"))
 
 
 	def test_closed(self):
@@ -29,4 +29,5 @@ class IsOpenTest(object):
 if __name__ == '__main__':
 	print "Please enter a locu api key"
 	KEY = sys.stdin.readline().strip()
+	print KEY
 	unittest.main()
